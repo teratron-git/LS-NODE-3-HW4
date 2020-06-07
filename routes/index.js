48612578025d793ba2f0779fd28048762eff8aae
Tereshkov Sergei[D] <rat87@mail.ru>
2020-06-07 22:59:48 +0300

    feat: rewrite router to Koa

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,29 @@
-const express = require('express');
-const router = express.Router();
+const Router = require('koa-router');
+const router = new Router();
+const koaBody = require('koa-body');
 const mainController = require('../controllers/mainController');
 const loginController = require('../controllers/loginController');
 const adminController = require('../controllers/adminController');
 const isAdmin = require('../utils/isAdmin');
 
 router.get('/', mainController.get);
-router.post('/', mainController.post);
+router.post('/', koaBody(), mainController.post);
 
 router.get('/login', loginController.get);
-router.post('/login', loginController.post);
+router.post('/login', koaBody(), loginController.post);
 
 router.get('/admin', isAdmin, adminController.get);
-router.post('/admin/upload', isAdmin, adminController.upload);
-router.post('/admin/skills', isAdmin, adminController.skills);
+router.post(
+  '/admin/upload',
+  isAdmin,
+  koaBody({
+    multipart: true,
+    formidable: {
+      uploadDir: process.cwd() + '/public/upload',
+    },
+  }),
+  adminController.upload
+);
+router.post('/admin/skills', isAdmin, koaBody(), adminController.skills);
 
 module.exports = router;
